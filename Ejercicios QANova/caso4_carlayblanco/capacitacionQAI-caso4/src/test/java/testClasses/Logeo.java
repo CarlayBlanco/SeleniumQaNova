6fd9f25580dffec99ec038805e3db6ca31f0d677
unknown <carlay.blanco@externos.bci.cl>
2022-05-18 19:10:11 -0400

    Se agrega ejercicio 4 de QANova 18-05-2022

---
+++ Ejercicios QANova/caso4_carlayblanco/capacitacionQAI-caso4/src/test/java/testClasses/Logeo.java
@@ -0,0 +1,43 @@
+package testClasses;
+
+import page.CargaInformacion;
+import page.DescargarArchivos;
+import page.Login;
+import page.MatrizInformacion;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Logeo {
+
+    private Login login;
+    private CargaInformacion cargaInformacion;
+    private MatrizInformacion matrizInformacion;
+    private DescargarArchivos descargarArchivos;
+
+    public Logeo(){
+    }
+
+    public void CasoLogin1(String usuario, String clave) throws ParseException {
+        login = new Login();
+        cargaInformacion = new CargaInformacion();
+        descargarArchivos = new DescargarArchivos();
+        login.validarTextoUsuario("Nombre del usuario:");
+        login.ingresarUsuario(usuario);
+        login.ingresarClave(clave);
+        login.clickBtnIngresar();
+        cargaInformacion.recuperarTitulo();
+        //Desde aca se agregan los llamados a las funciones que llenan el formulario
+        cargaInformacion.rellenarCampoTexto("Prueba 4 Carlay Blanco");
+        cargaInformacion.rellenarCampoCorreo("carlay.blanco@externos.bci.cl");//Agregada para evaluación 4
+        cargaInformacion.rellenarCampoAreaTexto("Esta es le evaluacion numero 4 realizada por Carlay Blanco");
+        cargaInformacion.rellenarFecha("16-11-1988");
+        cargaInformacion.rellenarLista("valor 2");
+        cargaInformacion.seleccionMultiple("2,3");
+        cargaInformacion.seleccionRadioButton(2);
+        cargaInformacion.seleccionarFechaCalendario("2020-11-16");
+        cargaInformacion.clickBtnEnviar();
+        //cargaInformacion.clickBtnReset();
+        //Hasta aca se agregan los llamados a las funciones que llenan el formulario
+        }
+}
